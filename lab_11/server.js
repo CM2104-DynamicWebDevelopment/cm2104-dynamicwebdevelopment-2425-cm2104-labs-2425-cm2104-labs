7b5be495d61fcf0b97fc288e956d38e338a2b8ea
jeshurun Olaide <sketchtitan7@gmail.com>
2025-01-30 11:46:12 +0000

    calculator

--- lab_11/server.js
+++ lab_11/server.js
@@ -11,5 +11,21 @@ app.get('/add',function(req,res){
     var y = req.query.y;
     res.send("X+Y="+(x+y));
 });
+app.get('/calc',function(req,res){
+    var x = req.query.x;
+    var y = req.query.y;
+    var operator =req.query.operator;
+    if(operator === "add"){
+        res.send(x+"+"+y+"="+ x+y);
+    }else if(operator === "sub"){
+        res.send(x+"-"+y+"="+ x-y);
+    }else if(operator === "div"){
+        res.send(x+"/"+y+"="+ x/y);
+    }else if(operator === "mul"){
+        res.send(x+"*"+y+"="+ x*y);
+}
+});
 
+        
+    
 app.listen(8080);
