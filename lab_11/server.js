542ec72aed0a73b72baff6d3d4a9fdb1c2fcd2db
jeshurun Olaide <sketchtitan7@gmail.com>
2025-01-30 11:11:45 +0000

    accesing parameters

--- lab_11/server.js
+++ lab_11/server.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 app.get('/', function(req,res){
     res.send("hwfar");
-})
+});
 app.get('/test',function(req,res){
     res.send("second route");
-})
+});
+app.get('/add',function(req,res){
+    var x = req.query.x;
+    var y = req.query.y;
+    res.send("X+Y="=(x+y));
+});
+
 app.listen(8080);
